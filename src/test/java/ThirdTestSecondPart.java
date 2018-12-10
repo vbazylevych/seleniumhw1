7d8a3100bc5c6229128d4b1a95252462fafdae97
vbazylevych <vbazylevych@playtika.com.ua>
2018-12-10 12:27:36 +0200

    fix comments

--- src/test/java/ThirdTestSecondPart.java
+++ src/test/java/ThirdTestSecondPart.java
@@ -10,12 +10,14 @@ public class ThirdTestSecondPart extends BaseTest {
     @Test
     public void h1IsPresent() {
         driver.get("http://localhost/litecart/");
-        List<WebElement> ducks = driver.findElements(By.className("image-wrapper"));
+        List<WebElement> ducks = driver.findElements(By.cssSelector(".product.column.shadow.hover-light"));
         ducks.forEach(duck -> checkSticer(duck));
     }
 
     private void checkSticer(WebElement duck) {
-        assertTrue(duck.findElements(By.cssSelector("[class*='sticker']")) != null);
+        List<WebElement> stickersForDuck = duck.findElements(By.cssSelector("[class*='sticker']"));
+        assertTrue(stickersForDuck != null);
+        assertTrue(stickersForDuck.size() == 1);
 
         //for me in order to ckeck
         // System.out.println(duck.findElements(By.cssSelector("[class*='sticker']")).get(0).getText());
