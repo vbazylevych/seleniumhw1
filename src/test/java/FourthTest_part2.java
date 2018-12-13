730a63989908651657061936acbd922e35a1342a
vbazylevych <608levlerka>
2018-12-13 21:49:46 +0200

    hw 4 part 2

--- src/test/java/FourthTest_part2.java
+++ src/test/java/FourthTest_part2.java
@@ -3,22 +3,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertTrue;
 
 
 public class FourthTest_part2 extends BaseTest {
 
     @Test
-    public void zonesForCanadaIsSorted() {
+    public void duckVsDuck() {
 
         goToMainPage();
-        WebElement firstDuck = driver.findElement(By.cssSelector("#box-campaigns .product.column.shadow.hover-light"));
+        WebElement firstDuck = driver.findElement(By.cssSelector("#box-campaigns [class^='product']"));
         String textOfDuckMainPage = firstDuck.findElement(By.className("name")).getAttribute("textContent");
         String regularPriceMainPage = firstDuck.findElement(By.className("regular-price")).getAttribute("textContent");
         String campaignPriceMainPage = firstDuck.findElement(By.className("campaign-price")).getAttribute("textContent");
-        String textDecorationMainPageCompainPrice = firstDuck.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
-        String colorMainPageRegularPrice = firstDuck.findElement(By.cssSelector(".regular-price")).getCssValue("color");
-        String colorMainPageCompainPrice = firstDuck.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
+        String textDecorationMainPageRegularPrice = firstDuck.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
+        String colorMainPageRegularPrice = firstDuck.findElement(By.cssSelector(".regular-price")).getCssValue("color").replaceAll(" ","");
+        String colorMainPageCompainPrice = firstDuck.findElement(By.cssSelector(".campaign-price")).getCssValue("color").replaceAll(" ","");
         String sizeFountMainPageRegularPrice = firstDuck.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
         String sizeFountMainPageCompainPrice = firstDuck.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
 
@@ -26,14 +29,44 @@ public class FourthTest_part2 extends BaseTest {
         String textOfDuckPage = driver.findElement(By.cssSelector("h1[class='title']")).getAttribute("textContent");
         String regularPriceDuckPage = driver.findElement(By.className("regular-price")).getAttribute("textContent");
         String campaignPriceDuckPage = driver.findElement(By.className("campaign-price")).getAttribute("textContent");
-        String textDecorationDuckPageCompainPrice = driver.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
-        String colorDuckPageRegularPrice = driver.findElement(By.cssSelector(".regular-price")).getCssValue("color");
-        String colorDuckPageCompainPrice = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
+        String textDecorationDuckPageRegularPrice = driver.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
+        String colorDuckPageRegularPrice = driver.findElement(By.cssSelector(".regular-price")).getCssValue("color").replaceAll(" ","");
+        String colorDuckPageCompainPrice = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("color").replaceAll(" ","");
         String sizeFountDuckPageRegularPrice = driver.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
-        String sizeFountDuckPageCompainPrice = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        String sizeFountDuckPageCampaignPrice = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        String boldFountMainPageCampaignPrice = driver.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
         System.out.println();
 
-       assertThat(textOfDuckMainPage.toLowerCase(), equalTo(textOfDuckPage.toLowerCase()));
+        assertThat(textOfDuckMainPage.toLowerCase(), equalTo(textOfDuckPage.toLowerCase()));
+        assertThat(regularPriceMainPage, equalTo(regularPriceDuckPage));
+        assertThat(campaignPriceMainPage, equalTo(campaignPriceDuckPage));
+        assertThat(textDecorationMainPageRegularPrice, containsString("line-through"));
+        assertThat(textDecorationDuckPageRegularPrice, containsString("line-through"));
+        String[] greyColorsDuckPage = colorDuckPageRegularPrice.substring(5, colorDuckPageRegularPrice.length() - 1)
+                .split(",");
+        assertThat(greyColorsDuckPage[0], equalTo(greyColorsDuckPage[1]));
+        assertThat(greyColorsDuckPage[0], equalTo(greyColorsDuckPage[2]));
+
+        String[] greyColorsDuckMainPage = colorMainPageRegularPrice.substring(5, colorDuckPageRegularPrice.length() - 1)
+                .split(",");
+        assertThat(greyColorsDuckMainPage[0], equalTo(greyColorsDuckMainPage[1]));
+        assertThat(greyColorsDuckMainPage[0], equalTo(greyColorsDuckMainPage[2]));
+
+       assertThat(boldFountMainPageCampaignPrice, containsString("700"));
+
+        String[] redColorsMainPage = colorDuckPageCompainPrice.substring(5, colorDuckPageCompainPrice.length() - 1)
+                .split(",");
+        assertThat(redColorsMainPage[0], not( equalTo(redColorsMainPage[1])));
+        assertThat(redColorsMainPage[1], equalTo("0"));
+        assertThat(redColorsMainPage[2], equalTo("0"));
+
+        double doubleSizeFountDuckPageCampaignPrice = Double.parseDouble(sizeFountDuckPageCampaignPrice.substring(0, sizeFountDuckPageCampaignPrice.length() - 2));
+        double doubleSizeFountDuckPageRegularPrice = Double.parseDouble(sizeFountDuckPageRegularPrice.substring(0, sizeFountDuckPageRegularPrice.length() - 2));
+        assertTrue(doubleSizeFountDuckPageCampaignPrice > doubleSizeFountDuckPageRegularPrice);
+
+        double doubleSizeFountMainPageCampaignPrice = Double.parseDouble(sizeFountMainPageCompainPrice.substring(0, sizeFountMainPageCompainPrice.length() - 2));
+        double doubleSizeFountMainPageRegularPrice = Double.parseDouble(sizeFountMainPageRegularPrice.substring(0, sizeFountMainPageCompainPrice.length() - 2));
+        assertTrue(doubleSizeFountMainPageCampaignPrice > doubleSizeFountMainPageRegularPrice);
 
     }
 
